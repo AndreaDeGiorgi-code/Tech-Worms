a771c3a46b6a43d75c42b63775dd0266c6adfa2d
andre <andrea.degiorgi@edu-its.it>
2025-05-27 12:48:52 +0200

    sistemato database config

--- backend/src/main/java/com/libreria/techbook/DatabaseConfig.java
+++ backend/src/main/java/com/libreria/techbook/DatabaseConfig.java
@@ -1,33 +1,26 @@
 package com.libreria.techbook;
-
+ 
 import javax.sql.DataSource;
-
+ 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-
-
+ 
 @Configuration
 public class DatabaseConfig {
-
+ 
     @Bean
     public DataSource dataSource() {
-        
-        String password="AVNS_gtzdZQp_tqmCcDF2CLW";
-        String username="avnadmin";
-
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
+ 
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-
+ 
         dataSource.setUrl("jdbc:mysql://mysql-techbook-techbook-db.d.aivencloud.com:18759/javadb"
             + "?useSSL=true&requireSSL=true&serverTimezone=UTC&allowPublicKeyRetrieval=true");
-
-        // Recupera da variabili d'ambiente
-        dataSource.setUsername(System.getenv("username")); 
-        dataSource.setPassword(System.getenv("password"));
-
+ 
+        dataSource.setUsername("avnadmin"); // utente Aiven
+        dataSource.setPassword("AVNS_gtzdZQp_tqmCcDF2CLW"); // password Aiven
+ 
         return dataSource;
     }
 }
